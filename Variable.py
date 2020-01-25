06decfc185842fa18faac3ed0ca87a39f3ae79f3
hlainghpone <hlainghpone647373@gmail.com>
2020-01-25 12:06:27 +0630

    Variable

---
+++ Variable.py
@@ -0,0 +1,55 @@
+print("hello world")
+>>> 2 + 2
+4
+>>> 50 - 5 * 6
+20
+>>> (50 - 5 * 6)/4
+5.0
+>>> round(5.0)
+5
+>>> round(5.023456,3)
+5.023
+>>>
+int - integer - 1,2,3, ---->308930 etc
+float -       - 1.0,5.9,6.0 63.29, etc
+string -      - 'Hello',"World","A","B",
+
+a = 1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+volume = width * height
+volume
+
+print('spam eggs')
+print(' don\'t')
+print("doesn't")
+print('"Yes"')
+print('\"Yes,\" they said.')
+print('"Isn\'t, they said')
+
+print('"Isn\'t, they said"')
+"'Hello World'"
+
+* / % + -(operator)
+
+addation = 75 + 75
+subteeaction = 204 - 204 
+multiplication = 100 * 0.5
+divison = 90 / 9
+power of 2 = 3 ** 2
+the remainder of the division = 10 % 3
+
+* or % 
+5 * 3 + 2 % 3
+
+5 * 3 + 3 % 4
+
+6 * 9 - 4 * 5 % 2
+
+7 * 5 - 6 * 3 % 3
+
+6 * 5 - 7 * 4 / 4 % 2
+
+8 * 9 / 4 * 5 % 3 - 2
